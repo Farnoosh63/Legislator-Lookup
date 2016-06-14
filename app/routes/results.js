e6e455e9a91f66f34dfdbe219ee182672ece7a63
farnoosh <farnoosh@pdx.edu>
2016-06-14 09:41:18 -0700

    add .env file

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,8 +1,11 @@
+import config from '../config/environment';
 import Ember from 'ember';
+
 
 export default Ember.Route.extend({
   model: function(params) {
-   var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=533b55cca3194420997cf563616c73d2&zip=' + params.zip;
+    var key = config.myApiKey;
+   var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey='+key+'&zip=' + params.zip;
    return Ember.$.getJSON(url).then(function(responseJSON) {
      return responseJSON.results;
    });
